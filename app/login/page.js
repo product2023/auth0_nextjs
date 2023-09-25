3e00629923a329b5c051ff677b8eadf6326d5e15
product2023 <132627302+product2023@users.noreply.github.com>
2023-09-25 14:33:32 +0530

    Update page.js

--- app/login/page.js
+++ app/login/page.js
@@ -3,7 +3,7 @@ import React from 'react'
 function page() {
   return (
     <div>
-        <a href="/api/auth/login">Login</a>
+        <a href="/api/auth/login">Click here to Login</a>
     </div>
   )
 }
