24b61eeaf8144efeb70460d48b9188cee94d8f22
product2023 <132627302+product2023@users.noreply.github.com>
2023-09-25 14:33:46 +0530

    Update page.js

--- app/logout/page.js
+++ app/logout/page.js
@@ -3,7 +3,7 @@ import React from 'react'
 function page() {
   return (
     <div>
-        <a href="/api/auth/logout">Logout</a>
+        <a href="/api/auth/logout">Click here to Logout</a>
     </div>
   )
 }
